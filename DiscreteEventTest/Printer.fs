16c26c4397602fa6a28e22c69879f88f26bdb61c
Matthew <matthewcrews@gmail.com>
2021-03-24 21:43:06 -0700

    Updating printer

--- DiscreteEventTest/Printer.fs
+++ DiscreteEventTest/Printer.fs
@@ -24,18 +24,26 @@ let history (state: State) =
         | FactType.Allocated (p, a, r) -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "Allocated"; ""|])
         | FactType.AllocationRequested a -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); a.ProcedureId.ToString(); "AllocatedRequested"; ""|])
         | FactType.Freed (p, a, r) -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "Freed"; ""|])
+        | FactType.ProcedureStarted p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "ProcedureStarted"; ""|])
+        | FactType.ProcedureCompleted p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "ProcedureCompleted"; ""|])
+        | FactType.Failed f -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); ""; "Failed"; ""|])
+        | FactType.Restored r -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); ""; "Restored"; ""|])
+        | FactType.Resumed p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "Restored"; ""|])
+        | FactType.Suspended p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "Restored"; ""|])
         | FactType.StepStarted (p, s, step) -> 
             match step.StepType with
             | StepType.Allocate _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepStarted"; "Allocate" |])
             | StepType.Delay _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepStarted"; "Delay" |])
             | StepType.Free _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepStarted"; "Free" |])
+            | StepType.Fail _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepStarted"; "Fail" |])
+            | StepType.Restore _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepStarted"; "Restore" |])
         | FactType.StepCompleted (p, s, step) -> 
             match step.StepType with
             | StepType.Allocate _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepCompleted"; "Allocate" |])
             | StepType.Delay _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepCompleted"; "Delay" |])
             | StepType.Free _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepCompleted"; "Free" |])
-        | FactType.ProcedureStarted p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "ProcedureStarted"; ""|])
-        | FactType.ProcedureCompleted p -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "ProcedureCompleted"; ""|])
+            | StepType.Fail _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepCompleted"; "Fail" |])
+            | StepType.Restore _ -> table.AddRow([|fact.FactId.ToString(); fact.TimeStamp.ToString(); p.ToString(); "StepCompleted"; "Restore" |])
         |> ignore
 
 
